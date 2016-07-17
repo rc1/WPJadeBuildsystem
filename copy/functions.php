51e5d1b8890f2075172f2cfc6126be2425d27c18
Ross Cairns <ross@electricglen.com>
2016-07-17 20:40:46 +0100

    Better defaults

--- copy/functions.php
+++ copy/functions.php
@@ -41,7 +41,7 @@ function remove_admin_bar_links () {
     $wp_admin_bar->remove_menu( 'wp-logo' );
     $wp_admin_bar->remove_menu( 'updates' );
     // $wp_admin_bar->remove_menu( 'customize' );
-    $wp_admin_bar->remove_menu( 'site-name' );
+    // $wp_admin_bar->remove_menu( 'site-name' );
 }
 
 add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
